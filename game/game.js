600735ae4add45e5348d905c626e74d0be8eede4
Samson <stanmoon95@gmail.com>
2017-10-28 23:50:53 -0400

    Unified planet creation
    Also remvoved all the semicolons

--- game/game.js
+++ game/game.js
@@ -46455,34 +46455,34 @@ class Random
 
 module.exports = new Random()
 },{"seedrandom":218}],233:[function(require,module,exports){
-require('pixi.js');
-const Viewport = require('pixi-viewport');
+require('pixi.js')
+const Viewport = require('pixi-viewport')
 // Prevents error from pixi-keyboard and pivi-particles
-window.PIXI = PIXI;
-window.PIXI["default"] = PIXI;
-// const Keyboard = require('pixi-keyboard');
-require('pixi-particles');
-require('pixi-keyboard');
+window.PIXI = PIXI
+window.PIXI['default'] = PIXI
+// const Keyboard = require('pixi-keyboard')
+require('pixi-particles')
+require('pixi-keyboard')
 
 function updateKeyboard() {
     /*
     if (PIXI.keyboardManager.isPressed(Key.DOWN)) {
-        console.log('Down key is pressed');
+        console.log('Down key is pressed')
     }*/
 
-    shift = PIXI.keyboardManager.isDown(Key.SHIFT);
+    shift = PIXI.keyboardManager.isDown(Key.SHIFT)
 
     if (PIXI.keyboardManager.isPressed(Key.UP)) {
 
         //_viewport.on('snap-end', () => addCounter('snap-end'))
-        viewport.plugins['decelerate'].reset();
+        viewport.plugins['decelerate'].reset()
         viewport.snap(0, 0, {
             time: 1000,
             removeOnComplete: true,
             center: true,
             ease: 'easeOutQuart'
-        });
-        viewport.removePlugin('follow');
+        })
+        viewport.removePlugin('follow')
     }
 
     if (PIXI.keyboardManager.isPressed(Key.RIGHT)) {
@@ -46496,44 +46496,44 @@ function updateKeyboard() {
                 x: 0,
                 y: 0
             }
-        });
+        })
     }
 
     if (PIXI.keyboardManager.isPressed(Key.DOWN)) {
-        viewport.removePlugin('snap');
+        viewport.removePlugin('snap')
     }
 
-    PIXI.keyboardManager.update();
+    PIXI.keyboardManager.update()
 }
 
-//document.addEventListener("mousewheel", mouseWheelHandler, false);
+//document.addEventListener('mousewheel', mouseWheelHandler, false)
 
-const maxHeight = 1000;
-const minHeight = 100;
+const maxHeight = 1000
+const minHeight = 100
 
-window.addEventListener('resize', resize);
-window.onorientationchange = resize;
+window.addEventListener('resize', resize)
+window.onorientationchange = resize
 
 // h is the constant height of the viewport.
-const h = 600;
+const h = 600
 // w is less important because the width can change. Mostly just used for initializing other objects
-const w = 600;
+const w = 600
 
 // Creates the PIXI application
 var game = new PIXI.Application(w, h, {
     antialias: true,
     transparent: false
-});
+})
 
 // Sets up the element
-game.view.style.position = "absolute";
-game.view.style.display = "block";
-document.body.appendChild(game.view);
-game.renderer.autoResize = true;
-game.renderer.backgroundColor = Colour.background;
+game.view.style.position = 'absolute'
+game.view.style.display = 'block'
+document.body.appendChild(game.view)
+game.renderer.autoResize = true
+game.renderer.backgroundColor = Colour.background
 
 // Adds the keyboard update loop to the ticker
-game.ticker.add(updateKeyboard);
+game.ticker.add(updateKeyboard)
 
 // Viewport options. Not very important because it can vary (see resize() )
 // These are mostly just used for initialization so that no errors occur
@@ -46543,16 +46543,16 @@ var options = {
     screenHeight: h,
     worldWidth: w,
     worldHeight: h,
-};
+}
 
-var viewport = new Viewport(game.stage, options);
+var viewport = new Viewport(game.stage, options)
 
 var clampOptions = {
     minWidth: 1,
     minHeight: minHeight,
     maxWidth: 1000 * w,
     maxHeight: maxHeight
-};
+}
 
 viewport
     .drag()
@@ -46562,250 +46562,211 @@ viewport
     })
     .clampZoom(clampOptions)
     .decelerate()
-//.start();
 
 function stopSnap() {
-    viewport.removePlugin('snap');
-    viewport.removePlugin('snap-zoom');
+    viewport.removePlugin('snap')
+    viewport.removePlugin('snap-zoom')
 }
 
 viewport.on('drag-start', function (e) {
-    stopSnap();
+    stopSnap()
 
-    viewport.removePlugin('follow');
-});
-viewport.on('pinch-start', stopSnap);
-viewport.on('wheel', stopSnap);
+    viewport.removePlugin('follow')
+})
+viewport.on('pinch-start', stopSnap)
+viewport.on('wheel', stopSnap)
 viewport.on('click', function (e) {
-    stopSnap();
+    stopSnap()
 
-    var planet = getPlanet(e.world.x, e.world.y);
+    var planet = getPlanet(e.world.x, e.world.y)
     if (planet) {
-        const animTime = 200;
+        const animTime = 200
 
-        this.doTheZoom = shift;
+        this.doTheZoom = shift
 
         // The calculated future positions of the planet
-        var x = Math.cos(((planet.age + (animTime / 1000)) * planet.speed) / planet.orbit.radius) * planet.orbit.radius;
-        var y = Math.sin(((planet.age + (animTime / 1000)) * planet.speed) / planet.orbit.radius) * planet.orbit.radius;
+        var x = Math.cos(((planet.age + (animTime / 1000)) * planet.speed) / planet.orbit.radius) * planet.orbit.radius
+        var y = Math.sin(((planet.age + (animTime / 1000)) * planet.speed) / planet.orbit.radius) * planet.orbit.radius
 
         viewport.snap(x, y, {
             time: animTime,
             removeOnComplete: true,
             center: true,
             ease: 'easeOutCirc'
-        });
+        })
 
         viewport.on('snap-end', function () {
-            viewport.follow(planet);
+            viewport.follow(planet)
 
             if (this.doTheZoom) {
-                this.doTheZoom = false;
+                this.doTheZoom = false
                 viewport.snapZoom({
                     time: (animTime * 2),
                     removeOnComplete: true,
                     ease: 'easeOutCirc',
                     direction: 'y'
-                }, 150);
+                }, 150)
             }
         })
     } else {
-        viewport.removePlugin('follow');
+        viewport.removePlugin('follow')
     }
-});
+})
 
-var planets;
+var planets
 
 // The extra pixels to add to the radius of a planet to determine whether to select it when clicked
-const clickThreshold = 40;
+const clickThreshold = 40
 
 function getPlanet(x, y) {
     for (var i in planets) {
-        var distSqr = ((x - planets[i].x) * (x - planets[i].x)) + ((y - planets[i].y) * (y - planets[i].y));
+        var distSqr = ((x - planets[i].x) * (x - planets[i].x)) + ((y - planets[i].y) * (y - planets[i].y))
         if (distSqr < ((planets[i].radius + clickThreshold) * (planets[i].radius + clickThreshold))) {
-            return planets[i];
+            return planets[i]
         }
     }
 
-    return null;
+    return null
 }
 
 PIXI.loader
     .add('sunTexture', 'game/sun.png')
     .add('planet1', 'game/planet1.png')
     .add('planet2', 'game/planet2.png')
-    .load(onLoad);
+    .load(onLoad)
+
+const G = 6.67 * 0.00000000001 // Gravitational constant
+const ppm = 0.0004 // pixels per meter
+const starMass = 2188000000000000000000000000000 // kg
+
+function createPlanet(texture, orbit, scale, mass) {
+    var planet = new PIXI.Sprite(texture)
+    planet.radius = 0.5 * planet.width
+    planet.orbit = orbit
+    planet.pivot.set(planet.radius, planet.radius)
+    planet.scale.set(scale)
+    planet.radius = planet.radius * planet.scale.x
+    planet.age = 0
+    planet.position.set(orbit.radius, 0)
+    planet.mass = mass
+    planet.speed = Math.sqrt((G * planet.mass) / (planet.radius / ppm)) * ppm
+    return planet
+}
 
 function onLoad(loader, resources) {
 
-    var orbit1 = game.stage.addChild(dottedCircle(0, 0, 150, 25));
-    var orbit2 = game.stage.addChild(dottedCircle(0, 0, 220, 25));
-    var orbit3 = game.stage.addChild(dottedCircle(0, 0, 270, 25));
-    var orbit4 = game.stage.addChild(dottedCircle(0, 0, 350, 25));
+    var orbit1 = game.stage.addChild(dottedCircle(0, 0, 150, 25))
+    var orbit2 = game.stage.addChild(dottedCircle(0, 0, 220, 25))
+    var orbit3 = game.stage.addChild(dottedCircle(0, 0, 270, 25))
+    var orbit4 = game.stage.addChild(dottedCircle(0, 0, 350, 25))
 
-    resize();
+    resize()
 
-    var sun = new PIXI.particles.Emitter(game.stage, resources.sunTexture.texture, sunParticle);
-    sun.emit = true;
+    var sun = new PIXI.particles.Emitter(game.stage, resources.sunTexture.texture, sunParticle)
+    sun.emit = true
 
-    var planet1 = new PIXI.Sprite(resources.planet1.texture);
-    planet1.radius = 0.5 * planet1.width;
-    planet1.orbit = orbit1;
-    planet1.pivot.set(planet1.radius, planet1.radius);
-    planet1.scale.set(0.1);
-    planet1.radius = planet1.radius * planet1.scale.x;
-    planet1.age = 0;
-    planet1.position.x = orbit1.radius;
-    game.stage.addChild(planet1);
+    var planet1 = game.stage.addChild(createPlanet(resources.planet1.texture, orbit1, 0.1, 4867000000000000000000000))
+    var planet2 = game.stage.addChild(createPlanet(resources.planet2.texture, orbit2, 0.1, 5972000000000000000000000))
+    var planet3 = game.stage.addChild(createPlanet(resources.planet1.texture, orbit3, 0.1, 3639000000000000000000000))
+    var planet4 = game.stage.addChild(createPlanet(resources.planet2.texture, orbit4, 0.1, 7568300000000000000000000))
 
-    var planet2 = new PIXI.Sprite(resources.planet2.texture);
-    planet2.radius = 0.5 * planet2.width;
-    planet2.orbit = orbit2;
-    planet2.pivot.set(planet2.radius, planet2.radius);
-    planet2.scale.set(0.1);
-    planet2.radius = planet2.radius * planet2.scale.x;
-    planet2.age = 0;
-    planet2.position.x = orbit2.radius;
-    game.stage.addChild(planet2);
+    planets = [planet1, planet2, planet3, planet4]
 
-    var planet3 = new PIXI.Sprite(resources.planet1.texture);
-    planet3.radius = 0.5 * planet3.width;
-    planet3.orbit = orbit3;
-    planet3.pivot.set(planet3.radius, planet3.radius);
-    planet3.scale.set(0.1);
-    planet3.radius = planet3.radius * planet3.scale.x;
-    planet3.age = 0;
-    planet3.position.x = orbit3.radius;
-    game.stage.addChild(planet3);
-
-    var planet4 = new PIXI.Sprite(resources.planet2.texture);
-    planet4.radius = 0.5 * planet4.width;
-    planet4.orbit = orbit4;
-    planet4.pivot.set(planet4.radius, planet4.radius);
-    planet4.scale.set(0.1);
-    planet4.radius = planet4.radius * planet4.scale.x;
-    planet4.age = 0;
-    planet4.position.x = orbit4.radius;
-    game.stage.addChild(planet4);
-
-
-    planets = [planet1, planet2, planet3, planet4];
-
-    const G = 6.67 * 0.00000000001;
-    const ppm = 0.0004;
-    const starMass = 2188000000000000000000000000000; // kg
-    planet1.mass = 4867000000000000000000000;
-    planet1.speed = Math.sqrt((G * planet1.mass) / (planet1.radius / ppm)) * ppm;
-
-    planet2.mass = 5972000000000000000000000;
-    planet2.speed = Math.sqrt((G * planet2.mass) / (planet2.radius / ppm)) * ppm;
-
-    planet3.mass = 3639000000000000000000000;
-    planet3.speed = Math.sqrt((G * planet3.mass) / (planet3.radius / ppm)) * ppm;
-
-    planet4.mass = 7568300000000000000000000;
-    planet4.speed = Math.sqrt((G * planet4.mass) / (planet4.radius / ppm)) * ppm;
-    // var v = sqrt(G * m_planet / radius)
-
-    this.lastElapsed = Date.now();
+    this.lastElapsed = Date.now()
     game.ticker.add(function () {
 
-        viewport.update();
+        viewport.update()
 
-        var now = Date.now();
-        var elasped = now - lastElapsed;
-        lastElapsed = now;
-        sun.update(elasped * 0.001);
+        var now = Date.now()
+        var elasped = now - lastElapsed
+        lastElapsed = now
+        let eTime = (elasped * 0.001)
+
+        sun.update(eTime)
 
         // Rotate the orbits (purely for visual effects)
-        orbit1.rotation += (elasped * 0.001) / 50;
-        orbit2.rotation += (elasped * 0.001) / 80;
-        orbit3.rotation += (elasped * 0.001) / 140;
-        orbit4.rotation += (elasped * 0.001) / 200;
+        orbit1.rotation += eTime / 50
+        orbit2.rotation += eTime / 80
+        orbit3.rotation += eTime / 140
+        orbit4.rotation += eTime / 200
 
-        planet1.age += (elasped * 0.001);
-        planet1.rotation = -planet1.age / 4;
-        planet1.position.x = Math.cos((planet1.age * planet1.speed) / orbit1.radius) * orbit1.radius;
-        planet1.position.y = Math.sin((planet1.age * planet1.speed) / orbit1.radius) * orbit1.radius;
+        for (i in planets) {
+            planets[i].age += eTime;
+            let radius = planets[i].orbit.radius
+            let x = Math.cos((planets[i].age * planets[i].speed) / radius) * radius
+            let y = Math.sin((planets[i].age * planets[i].speed) / radius) * radius
+            planets[i].position.set(x, y)
+        }
 
-        planet2.age += (elasped * 0.001);
-        planet2.rotation = -planet2.age / 6;
-        planet2.position.x = Math.cos((planet2.age * planet2.speed) / orbit2.radius) * orbit2.radius;
-        planet2.position.y = Math.sin((planet2.age * planet2.speed) / orbit2.radius) * orbit2.radius;
+        // TODO add these to the createPlanet stuff
+        planet1.rotation = -planet1.age / 4
+        planet2.rotation = -planet2.age / 6
+        planet3.rotation = planet3.age / 3
+        planet4.rotation = -planet4.age / 2
+    })
 
-        planet3.age += (elasped * 0.001);
-        planet3.rotation = planet3.age / 3;
-        planet3.position.x = Math.cos((planet3.age * planet3.speed) / orbit3.radius) * orbit3.radius;
-        planet3.position.y = Math.sin((planet3.age * planet3.speed) / orbit3.radius) * orbit3.radius;
-
-        planet4.age += (elasped * 0.001);
-        planet4.rotation = -planet4.age / 2;
-        planet4.position.x = Math.cos((planet4.age * planet4.speed) / orbit4.radius) * orbit4.radius;
-        planet4.position.y = Math.sin((planet4.age * planet4.speed) / orbit4.radius) * orbit4.radius;
-    });
-
-    viewport.moveCenter(0, 0);
+    viewport.moveCenter(0, 0)
 }
 
 function resize() {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0)
 
-    var oldCenter;
+    var oldCenter
     if (viewport.center) {
-        oldCenter = viewport.center;
+        oldCenter = viewport.center
     }
 
-    var prevHeight = viewport.worldScreenHeight;
+    var prevHeight = viewport.worldScreenHeight
 
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var ratio = height / h;
+    var width = window.innerWidth
+    var height = window.innerHeight
+    var ratio = height / h
 
-    game.renderer.resize(width, height);
-    viewport.resize(width, height);
-    viewport.fitHeight(prevHeight, false);
+    game.renderer.resize(width, height)
+    viewport.resize(width, height)
+    viewport.fitHeight(prevHeight, false)
 
     // Must maintain the center manually instad of using fitHeight's built in one because the
     // center value will change upon resizing the viewport and game window
     if (oldCenter) {
-        viewport.moveCenter(oldCenter);
+        viewport.moveCenter(oldCenter)
     }
 
-    stopSnap();
+    stopSnap()
 }
 
-const minDashes = 2;
-const dashThickness = 1.4;
+const minDashes = 2
+const dashThickness = 1.4
 
 function dottedCircle(x, y, radius, dashLength) {
 
-    var numOfDashes = Math.max(Math.floor(Math.PI * radius / dashLength), minDashes);
-    var dashRadians = dashLength / radius;
-    var spacingRadians = (2 * Math.PI / numOfDashes) - dashRadians;
+    var numOfDashes = Math.max(Math.floor(Math.PI * radius / dashLength), minDashes)
+    var dashRadians = dashLength / radius
+    var spacingRadians = (2 * Math.PI / numOfDashes) - dashRadians
 
-    var pixiCircle = new PIXI.Graphics();
-    pixiCircle.radius = radius;
+    var pixiCircle = new PIXI.Graphics()
+    pixiCircle.radius = radius
 
     // If it's a full circle, draw it full (more optimised)
     if (spacingRadians <= 0) {
-        pixiCircle.lineStyle(dashThickness, Colour.dashedLine); //(thickness, color)
-        pixiCircle.arc(x, y, radius, 0, 2 * Math.PI);
+        pixiCircle.lineStyle(dashThickness, Colour.dashedLine) //(thickness, color)
+        pixiCircle.arc(x, y, radius, 0, 2 * Math.PI)
     } else { // Else, draw it dashed
         for (i = 0; i < numOfDashes; i++) {
-            var start = i * (dashRadians + spacingRadians);
-            var end1 = start + dashRadians;
-            var end2 = end1 + spacingRadians;
-            pixiCircle.lineStyle(dashThickness, Colour.dashedLine); //(thickness, color)
-            pixiCircle.arc(x, y, radius, start, end1);
-            pixiCircle.lineStyle(dashThickness, Colour.background, 0);
-            pixiCircle.arc(x, y, radius, end1, end2);
+            var start = i * (dashRadians + spacingRadians)
+            var end1 = start + dashRadians
+            var end2 = end1 + spacingRadians
+            pixiCircle.lineStyle(dashThickness, Colour.dashedLine) //(thickness, color)
+            pixiCircle.arc(x, y, radius, start, end1)
+            pixiCircle.lineStyle(dashThickness, Colour.background, 0)
+            pixiCircle.arc(x, y, radius, end1, end2)
         }
     }
 
     // disgusting
-    // pixiCircle.cacheAsBitmap = true;
+    // pixiCircle.cacheAsBitmap = true
 
-    return pixiCircle;
+    return pixiCircle
 }
 
 },{"pixi-keyboard":47,"pixi-particles":48,"pixi-viewport":49,"pixi.js":177}]},{},[233]);
